2b52c779595bb1a2c0a70e149dda66a967632fe0
Payam Zahedi <pzahedi1995@gmail.com>
2024-07-21 19:54:34 +0200

    Built example/web
    Branch: main
    Commit: 9c2c8ae85b0c2f110810f56f20b328ab3d55cc9a
    
    package:peanut 5.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3905870380"
+    serviceWorkerVersion: "3582686166"
   }
 });
